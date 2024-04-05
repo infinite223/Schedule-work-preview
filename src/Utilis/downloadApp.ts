a90356c1ca5ed5e2b979c0e7f1e8e7019195e15f
dawid.szmigiel <dawid.szmigiel@codefusion.pl>
2024-04-05 23:40:02 +0200

    update app

--- src/Utilis/downloadApp.ts
+++ src/Utilis/downloadApp.ts
@@ -1,9 +1,9 @@
 export const downloadApp = async () => {
-    const apkFilePath = '/ScheduleWork v1.0.2.apk';
+    const apkFilePath = '/ScheduleWork v1.0.3.apk';
 
     const anchor = document.createElement('a');
     anchor.href = apkFilePath;
-    anchor.download = 'ScheduleWork v1.0.2.apk';
+    anchor.download = 'ScheduleWork v1.0.3.apk';
 
     document.body.appendChild(anchor);
 
